6fa9bc644d818ab72398ab0758fe2b746ca11c49
IvanScriptS <iachavez@utp.edu.co>
2022-10-05 01:46:26 -0500

    Funcionado bien el repositorio, comprobado con Insomnia.

--- src/main/java/com/example/demo/services/TaskService.java
+++ src/main/java/com/example/demo/services/TaskService.java
@@ -2,23 +2,23 @@ package com.example.demo.services;
 
 import com.example.demo.entities.Task;
 import com.example.demo.entities.TaskList;
+import com.example.demo.repositories.TaskRepository;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-
+import java.util.List;
+@Service
 public class TaskService {
-    Task tarea1;
-    Task tarea2;
-    TaskList propositos;
+    private TaskRepository repository;
 
-    public TaskService(){
-        this.tarea1= new Task("Understanding Backend", Boolean.TRUE, LocalDate.of(2022,10,3));
-        this.tarea2= new Task("Understanding HTML", Boolean.TRUE, LocalDate.of(2022,10,4));
-        this.propositos=new TaskList("This is 2022 Proposes");
-        propositos.addTask(tarea1);
-        propositos.addTask(tarea2);
+    public TaskService(TaskRepository repository){
+        this.repository=repository;
     }
 
-    public TaskList getTaskList(){
-        return this.propositos;
+    public List<Task> getTaskList(){
+        return this.repository.findAll();
+    }
+    public Task createTask(Task newTask){
+        return this.repository.save(newTask);
     }
 }
